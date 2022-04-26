dff95ab2ca1a3037a419bb9f2a3ec70c3b98bf5b
rmatafonov <matafonov.rv@gmail.com>
2022-04-26 08:49:41 +0300

    Express fixes

--- src/server/server.js
+++ src/server/server.js
@@ -4,6 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static("./dist"));
+app.use('/*', express.static("./dist"));
 
 app.listen(PORT, function () {
   console.log(`Server running at http://localhost:${PORT}`);
